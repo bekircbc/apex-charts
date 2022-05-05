b4270d01db391d3a9eae059b1c337835b539a36b
Bekir - CDI <b.burakcebecii@gmail.com>
2022-05-05 09:18:26 +0200

    react-apexcharts installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
+import Chart from "react-apexcharts";
 import { useState } from "react";
-import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
+export const PageApex = () => {
+  const [options, setOptions] = useState({
+    chart: {
+      id: "apexchart-example",
+    },
+    xaxis: {
+      categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999],
+    },
+  });
 
-  return <div className="App">Hello!</div>;
-}
+  const [series, setSeries] = useState([
+    {
+      name: "series-1",
+      data: [30, 40, 35, 50, 49, 60, 70, 91, 125],
+    },
+    {
+      name: "series-2",
+      data: [20, 30, 55, 75, 63, 60, 80, 134, 110],
+    },
+  ]);
 
-export default App;
+  return (
+    <Chart
+      options={options}
+      series={series}
+      type="line"
+      width={800}
+      height={520}
+    />
+  );
+};
